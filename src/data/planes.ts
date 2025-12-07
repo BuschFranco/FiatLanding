7bfeacc60207d43060e6c6bf79ec08d7afe17a5b
BuschFranco <francobusch130@gmail.com>
2025-12-07 03:06:47 -0300

    Agrega modelos, página de modelos, formulario de contacto, etc

---
+++ src/data/planes.ts
@@ -0,0 +1,56 @@
+export const planes = [
+  {
+    marca: "Fiat",
+    precio: "$464.496",
+    modelo: "Cronos",
+    version: "DRIVE 1.3L MT5 PACK PLUS",
+    financiamiento: "Plan 90% financiado en 84 cuotas",
+    img: "/images/models/cronos.webp",
+    slug: "cronos",
+    precioVehiculo: 36429000,
+    specs: {
+      motor: "FireFly 1.3 nafta, 4 cilindros, 8 válvulas",
+      potencia: "99 CV @ 6.000 rpm",
+      torque: "127 Nm @ 4.000 rpm",
+      transmision: "Manual 5 velocidades",
+      traccion: "Delantera",
+      dim: "Sedán B; 0–100 km/h ~11.5 s; V. máx 174 km/h"
+    }
+  },
+  {
+    marca: "Fiat",
+    precio: "$304.162",
+    modelo: "Mobi",
+    version: "1,0 EASY PACK TOP",
+    financiamiento: "Plan 80% financiado en 84 cuotas",
+    img: "/images/models/mobi.png",
+    slug: "mobi",
+    precioVehiculo: 24500000,
+    specs: {
+      motor: "Fire 1.0 nafta, 4 cilindros, 8 válvulas",
+      potencia: "70 CV @ 6.250 rpm",
+      torque: "94 Nm @ ~3.850 rpm",
+      transmision: "Manual 5 velocidades",
+      traccion: "Delantera",
+      dim: "L 3.566 mm / W 1.633 mm / H 1.505 mm; Baúl 235 L"
+    }
+  },
+  {
+    marca: "Fiat",
+    precio: "$375.209",
+    modelo: "Argo",
+    version: "ENDURANCE CD 1.4",
+    financiamiento: "Plan 70% financiado en 84 cuotas",
+    img: "/images/models/argo.png",
+    slug: "argo",
+    precioVehiculo: 37520900,
+    specs: {
+      motor: "Fire 1.4 nafta, 4 cilindros, 8 válvulas",
+      potencia: "87 CV @ 5.750 rpm",
+      torque: "124 Nm @ 3.500 rpm",
+      transmision: "Manual 5 velocidades",
+      traccion: "Delantera",
+      dim: "L 4.000 mm / W 1.710 mm / H 1.505 mm; Baúl 300 L"
+    }
+  },
+];
